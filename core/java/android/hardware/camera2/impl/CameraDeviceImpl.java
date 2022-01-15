Evolution-X-AOSP/frameworks_base

commit 7e025e206dd4dca24749a118fd63432ae9e39e6e

Add config overlay to force enable multi resolution for camera
* Apparently, required by OOS camera port.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -129,6 +129,8 @@ public class CameraDeviceImpl extends CameraDevice
     private final int mTotalPartialCount;
     private final Context mContext;
 
+    private final boolean mForceMultiResolution;
+
     private static final long NANO_PER_SECOND = 1000000000; //ns
 
     /**
@@ -302,6 +304,9 @@ public class CameraDeviceImpl extends CameraDevice
             mTotalPartialCount = partialCount;
         }
         mIsPrivilegedApp = checkPrivilegedAppList();
+
+        mForceMultiResolution = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceMultiResolution);
     }
 
     public CameraDeviceCallbacks getCallbacks() {
@@ -493,7 +498,7 @@ public class CameraDeviceImpl extends CameraDevice
                     if (inputConfig != null) {
                         int streamId = mRemoteDevice.createInputStream(inputConfig.getWidth(),
                                 inputConfig.getHeight(), inputConfig.getFormat(),
-                                inputConfig.isMultiResolution());
+                                inputConfig.isMultiResolution() || mForceMultiResolution);
                         mConfiguredInput = new SimpleEntry<Integer, InputConfiguration>(
                                 streamId, inputConfig);
                     }
@@ -1525,7 +1530,7 @@ public class CameraDeviceImpl extends CameraDevice
             return;
         }
         int inputFormat = inputConfig.getFormat();
-        if (inputConfig.isMultiResolution()) {
+        if (inputConfig.isMultiResolution() || mForceMultiResolution) {
             MultiResolutionStreamConfigurationMap configMap = mCharacteristics.get(
                     CameraCharacteristics.SCALER_MULTI_RESOLUTION_STREAM_CONFIGURATION_MAP);
 

core/res/res/values/evolution_config.xml
@@ -137,4 +137,7 @@
     <!-- NightLight FOD -->
     <bool name="disable_fod_night_light">false</bool>
 
+    <!-- Whether to force enable multi resolution configuration for camera -->
+    <bool name="config_forceMultiResolution">false</bool>
+
 </resources>

core/res/res/values/evolution_symbols.xml
@@ -122,4 +122,7 @@
     <!-- NightLight FOD -->
     <java-symbol type="bool" name="disable_fod_night_light"/>
 
+    <!-- Whether to force enable multi resolution configuration for camera -->
+    <java-symbol type="bool" name="config_forceMultiResolution" />
+
 </resources>

jjpprrrr/frameworks_base-1

commit ff7264348315d45f233800a13dc566d89cebe6bc

fixup! Add config overlay to force enable multi resolution for camera
* Fixes: 7e025e206dd4dca24749a118fd63432ae9e39e6e

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -498,7 +498,7 @@ public class CameraDeviceImpl extends CameraDevice
                     if (inputConfig != null) {
                         int streamId = mRemoteDevice.createInputStream(inputConfig.getWidth(),
                                 inputConfig.getHeight(), inputConfig.getFormat(),
-                                inputConfig.isMultiResolution() || mForceMultiResolution);
+                                inputConfig.isMultiResolution());
                         mConfiguredInput = new SimpleEntry<Integer, InputConfiguration>(
                                 streamId, inputConfig);
                     }